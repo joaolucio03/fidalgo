86037b0173471df57a02127e1b1d61a1eb4b398b
João Lúcio <jpedro.boavida@gmail.com>
2024-02-29 14:51:00 +0000

    teste7

--- sketch.js
+++ sketch.js
@@ -1,11 +1,9 @@
 function setup() {
-    createCanvas(1920, 1080, WEBGL);
+    createCanvas(PictureInPictureWindow);
     background(255,0,0);
   }
   
   
   function draw() {
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    box(mouseY, mouseY);
+    circle(mouseX,mouseY,100)
   }
